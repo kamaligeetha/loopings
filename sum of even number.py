c49bb9ce28d371dbc425801e5e5ea8bfc18dbaf5
Kamalishwari.S <53109104+kamaligeetha@users.noreply.github.com>
2020-01-05 18:11:25 +0530

    Create sum of even number.py

---
+++ sum of even number.py
@@ -0,0 +1,9 @@
+msd = int(input())
+sum = 0
+ 
+for k in range(1, msd+1):
+    if(k % 2 == 0):
+        print("{0}".format(k))
+        sum = sum + k
+ 
+print("The Sum of Even Numbers are 1 to {0} = {1}".format(k,sum))
